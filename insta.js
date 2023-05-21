9b004e81311e989e30c028c8c76e91f494555aa5
Mosesomo <moseswamboga4@gmail.com>
2023-05-21 13:29:54 +0300

    car shop

---
+++ insta.js
@@ -0,0 +1,10 @@
+function openmenu(){
+    document.getElementById("header-list").style.display="block";
+    document.getElementById("open-btn").style.display="none";
+    document.getElementById("close-btn").style.display="block";
+}
+function closemenu(){
+    document.getElementById("header-list").style.display="none";
+    document.getElementById("open-btn").style.display="block";
+    document.getElementById("close-btn").style.display="none";
+}
